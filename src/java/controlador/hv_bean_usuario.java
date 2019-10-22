0930dc1765faf0fd65ce553da34d89ada80f1d69
FelipeCorrea06 <lfcorrea97@gmail.com>
2019-10-21 23:41:54 -0500

    creacion de carpetas y capas

---
+++ src/java/controlador/hv_bean_usuario.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Usuario
+ */
+@ManagedBean
+@RequestScoped
+public class hv_bean_usuario {
+
+    /**
+     * Creates a new instance of hv_bean_usuario
+     */
+    public hv_bean_usuario() {
+    }
+    
+}
